ddcded8ba6e0276958e8c3e1935b1c0ba013d9ba
Danish Rehman <76951685+Danishrehman786@users.noreply.github.com>
2024-12-18 00:58:15 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//Chapter No 1 Alert Messages
+
+//Question No 1
+//alert("A very warm welcome to you.");
+
+//Question No 2
+//alert("Error! Please enter valid password.");
+
+//Question No 3
+//alert("Welcome to JS Land \n Happy Coding");
+
+//Question No 4
+//alert("Welcome to JS Land");
+//alert("Happy Coding");
+
+//Question No 5
+//console.log("Hello..I can run JS through my web browsers console");
+
+
+
+//Chapter No 2 Variables for Strings
+
+//Question No 1
+
+//var myName= ("Danish Rehman");
+//alert(myName);
+
+//var message= ("Hello World");
+//alert(message);
+
+//var bioData= ("Certified Mobile Application Development");
+//alert(bioData);
+
+//Question No 2
+
+//var foodName=("Pizza\nPizz\nPiz\nPi\nP");
+//alert(foodName);
+
+//Question No 3
+
+//var email=("Danishrehman51@gmail.com");
+//alert(email);
+
+//Question No 4
+
+//var book=("A smarter way to learn Java Script");
+//alert(book);
+
+//Question No 5
+
+//document.write("<h1>Thank you for choosing us as your next career move.We're very proud to be welcoming you to our company.</h1>")
+
+//Chapter No 3 Variables for Numbers var and num
+
+//Question No 1
+
+// var age = (26);
+// alert(age);
+
+//Question No 2
+
+//var visitors=("You have visited this site 14 times.");
+//alert(visitors);
+
+//Question No 3
+
+//var birthYear="1997"
+//document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+
+//var customerName = (" Mr Danish ordered \n");
+//var quantity = (5);
+//var product = ("T-shirt(s) on \n");
+//var store = ("Danish Brand")
+
+//document.write(customerName + quantity + product + store)
